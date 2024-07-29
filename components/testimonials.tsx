df61fb420a7791199d9c4d2ca3c025c08e1e5f41
Oyegurman <oyegurman@gmail.com>
2024-07-30 01:05:54 +0530

    Changed the content for About us and added buttons in fund management and account management

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -3,22 +3,13 @@ import Image from "next/image";
 export default function Testimonials() {
   return (
     <section>
-      <div className='max-w-6xl mx-auto px-4 sm:px-6 bg-green-600'>
+      <div className='max-w-6xl mx-auto px-4 sm:px-6 bg-green-700'>
         <div className='py-12 md:py-20 border-t border-gray-800 flex flex-col lg:flex-row items-center'>
           {/* Section header */}
           <div className='max-w-3xl mx-auto text-center lg:text-left lg:w-2/3 pb-12 md:pb-20'>
-            <h2 className='h2 mb-4 text-white'>About Us</h2>
-            <p className='text-xl text-gray-200'>
-              At Gainify, we are committed to helping our clients achieve their
-              financial goals by investing wisely in the financial markets. Our
-              mission is to provide strong returns and ensure financial freedom for
-              individuals, families, and companies alike. We understand the importance
-              of financial knowledge and aim to spread awareness to empower our
-              clients to make informed decisions. Many individuals face challenges in
-              the markets due to a lack of knowledge, and we're here to bridge that
-              gap. While we are at the beginning stage of our journey, we are
-              determined to do much more. Join us on this incredible adventure towards
-              financial success!
+            <h2 className='h2 mb-4 text-white'>About Gainify</h2>
+            <p className='text-xl text-white'>
+            Gainify is a fund management company that excels in navigating financial markets to achieve outstanding returns. We combine expert strategies with financial education to help clients reach their financial goals and attain long-term success.
             </p>
           </div>
 
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -6,209 +6,159 @@ import FeatImage03 from "@/public/images/features-03-image-03.png";
 
 export default function Zigzag() {
   return (
-			<section>
-				<div className='max-w-6xl mx-auto px-4 sm:px-6'>
-					<div className='py-12 md:py-20 border-t border-gray-800'>
-						{/* Section header */}
-						<div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
-							<h1 className='h2 mb-4'>Our Services</h1>
-							<p className='text-xl text-gray-400'>
-								Explore our comprehensive investment and account management services
-								designed to maximize your returns and provide top-tier financial
-								support.
-							</p>
-						</div>
+    <section>
+      <div className='max-w-6xl mx-auto px-4 sm:px-6'>
+        <div className='py-12 md:py-20 border-t border-gray-800'>
+          {/* Section header */}
+          <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
+            <h1 className='h2 mb-4'>Our Services</h1>
+            <p className='text-xl text-gray-400'>
+              Explore our comprehensive investment and account management services
+              designed to maximize your returns and provide top-tier financial
+              support.
+            </p>
+          </div>
 
-						{/* Items */}
-						<div className='grid gap-20'>
-							{/* 1st item */}
-							<div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
-								{/* Image */}
-								<div
-									className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1'
-									data-aos='fade-up'>
-									<Image
-										className='max-w-full mx-auto md:max-w-none h-auto'
-										src={FeatImage01}
-										width={540}
-										height={405}
-										alt='Features 01'
-									/>
-								</div>
-								{/* Content */}
-								<div
-									className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6'
-									data-aos='fade-right'>
-									<div className='md:pr-4 lg:pr-12 xl:pr-16'>
-										<div className='font-architects-daughter text-xl text-green-600 mb-2'>
-											"Greater gains. Smarter investments."
-										</div>
-										<h3 className='h3 mb-3'>Funds Management</h3>
-										<p className='text-xl text-gray-400 mb-4'>
-											Discover our tailored investment solutions, designed to maximize
-											returns and provide exceptional financial support:
-										</p>
-										<ul className='text-lg text-gray-400 -mb-2'>
-											<li className='flex items-center mb-2'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>Fund management offering returns from 8% to 220%</span>
-											</li>
-											<li className='flex items-center mb-2'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>100% profit retention with monthly withdrawals</span>
-											</li>
-											<li className='flex items-center'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>
-													Premier financial support to enhance your investment experience
-												</span>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
+          {/* Items */}
+          <div className='grid gap-20'>
+            {/* 1st item */}
+            <div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
+              {/* Image */}
+              <div
+                className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1'
+                data-aos='fade-up'>
+                <Image
+                  className='max-w-full mx-auto md:max-w-none h-auto'
+                  src={FeatImage01}
+                  width={540}
+                  height={405}
+                  alt='Features 01'
+                />
+              </div>
+              {/* Content */}
+              <div
+                className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6'
+                data-aos='fade-right'>
+                <div className='md:pr-4 lg:pr-12 xl:pr-16'>
+                  <div className='font-architects-daughter text-xl text-green-600 mb-2'>
+                    "Greater gains. Smarter investments."
+                  </div>
+                  <h3 className='h3 mb-3'>Funds Management</h3>
+                  <p className='text-xl text-gray-400 mb-4'>
+                    Discover our tailored investment solutions, designed to maximize
+                    returns and provide exceptional financial support:
+                  </p>
+                  <ul className='text-lg text-gray-400 -mb-2'>
+                    <li className='flex items-center mb-2'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>Fund management offering returns from 8% to 220%</span>
+                    </li>
+                    <li className='flex items-center mb-2'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>100% profit retention with monthly withdrawals</span>
+                    </li>
+                    <li className='flex items-center'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>
+                        Premier financial support to enhance your investment experience
+                      </span>
+                    </li>
+                  </ul>
+                  <div className='text-center'>
+                    <button className='mt-8 px-4 py-2 bg-teal-500 text-white rounded-full'>
+                      Learn More
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-							{/* 2nd item */}
-							<div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
-								{/* Image */}
-								<div
-									className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl'
-									data-aos='fade-up'>
-									<Image
-										className='max-w-full mx-auto md:max-w-none h-auto'
-										src={FeatImage02}
-										width={540}
-										height={405}
-										alt='Features 02'
-									/>
-								</div>
-								{/* Content */}
-								<div
-									className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6'
-									data-aos='fade-left'>
-									<div className='md:pl-4 lg:pl-12 xl:pl-16'>
-										<div className='font-architects-daughter text-xl text-green-600 mb-2'>
-											"Greater gains. Smarter investments"
-										</div>
-										<h3 className='h3 mb-3'>Accounts Management</h3>
-										<p className='text-xl text-gray-400 mb-4'>
-											Experience our expert account management services, delivering
-											substantial returns and a profit-sharing model that aligns with your
-											success:
-										</p>
-										<ul className='text-lg text-gray-400 -mb-2'>
-											<li className='flex items-center mb-2'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>Potential returns of up to 10% monthly</span>
-											</li>
-											<li className='flex items-center mb-2'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>No upfront fees required</span>
-											</li>
-											<li className='flex items-center'>
-												<svg
-													className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-													viewBox='0 0 12 12'
-													xmlns='http://www.w3.org/2000/svg'>
-													<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-												</svg>
-												<span>
-													50-50 profit sharing: we only earn when we generate profits in your
-													account
-												</span>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							{/* 3rd item see below end of file commented third item*/}
-						</div>
-					</div>
-				</div>
-			</section>
-		);
+            {/* 2nd item */}
+            <div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
+              {/* Image */}
+              <div
+                className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl'
+                data-aos='fade-up'>
+                <Image
+                  className='max-w-full mx-auto md:max-w-none h-auto'
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt='Features 02'
+                />
+              </div>
+              {/* Content */}
+              <div
+                className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6'
+                data-aos='fade-left'>
+                <div className='md:pl-4 lg:pl-12 xl:pl-16'>
+                  <div className='font-architects-daughter text-xl text-green-600 mb-2'>
+                    "Greater gains. Smarter investments"
+                  </div>
+                  <h3 className='h3 mb-3'>Accounts Management</h3>
+                  <p className='text-xl text-gray-400 mb-4'>
+                    Experience our expert account management services, delivering
+                    substantial returns and a profit-sharing model that aligns with your
+                    success:
+                  </p>
+                  <ul className='text-lg text-gray-400 -mb-2'>
+                    <li className='flex items-center mb-2'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>Potential returns of up to 10% monthly</span>
+                    </li>
+                    <li className='flex items-center mb-2'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>No upfront fees required</span>
+                    </li>
+                    <li className='flex items-center'>
+                      <svg
+                        className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
+                        viewBox='0 0 12 12'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
+                      </svg>
+                      <span>
+                        50-50 profit sharing: we only earn when we generate profits in your
+                        account
+                      </span>
+                    </li>
+                  </ul>
+                  <div className='text-center'>
+                    <button className='mt-8 px-4 py-2 bg-teal-500 text-white rounded-full'>
+                      Learn More
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-    // <div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
-		// 			{/* Image */}
-		// 			<div
-		// 				className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1'
-		// 				data-aos='fade-up'>
-		// 				<Image
-		// 					className='max-w-full mx-auto md:max-w-none h-auto'
-		// 					src={FeatImage03}
-		// 					width={540}
-		// 					height={405}
-		// 					alt='Features 03'
-		// 				/>
-		// 			</div>
-		// 			{/* Content */}
-		// 			<div
-		// 				className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6'
-		// 				data-aos='fade-right'>
-		// 				<div className='md:pr-4 lg:pr-12 xl:pr-16'>
-		// 					<div className='font-architects-daughter text-xl text-green-600 mb-2'>
-		// 						More speed. Less spend
-		// 					</div>
-		// 					<h3 className='h3 mb-3'>Keep projects on schedule</h3>
-		// 					<p className='text-xl text-gray-400 mb-4'>
-		// 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-		// 						tempor incididunt ut labore et dolore magna aliqua.
-		// 					</p>
-		// 					<ul className='text-lg text-gray-400 -mb-2'>
-		// 						<li className='flex items-center mb-2'>
-		// 							<svg
-		// 								className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-		// 								viewBox='0 0 12 12'
-		// 								xmlns='http://www.w3.org/2000/svg'>
-		// 								<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-		// 							</svg>
-		// 							<span>Duis aute irure dolor in reprehenderit</span>
-		// 						</li>
-		// 						<li className='flex items-center mb-2'>
-		// 							<svg
-		// 								className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-		// 								viewBox='0 0 12 12'
-		// 								xmlns='http://www.w3.org/2000/svg'>
-		// 								<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-		// 							</svg>
-		// 							<span>Excepteur sint occaecat</span>
-		// 						</li>
-		// 						<li className='flex items-center'>
-		// 							<svg
-		// 								className='w-3 h-3 fill-current text-green-500 mr-2 shrink-0'
-		// 								viewBox='0 0 12 12'
-		// 								xmlns='http://www.w3.org/2000/svg'>
-		// 								<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
-		// 							</svg>
-		// 							<span>Amet consectetur adipiscing elit</span>
-		// 						</li>
-		// 					</ul>
-		// 				</div>
-		// 			</div>
-		// 		</div>;
