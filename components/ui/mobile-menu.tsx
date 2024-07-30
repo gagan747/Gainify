fb042296b3dcb0bd5c69495b8aecec9f1488d854
gurjot74 <gagan.singh@tothenew.com>
2024-07-31 04:09:02 +0530

    added about us navigatio

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -71,20 +71,20 @@ export default function MobileMenu() {
         <ul className="bg-gray-800 px-4 py-2">
           <li>
             <Link
-              href="/signin"
+              href="/#testimonials"
               className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
               onClick={() => setMobileNavOpen(false)}
             >
-              Sign in
+              About us
             </Link>
           </li>
           <li>
             <Link
-              href="/signup"
+              href="/signin"
               className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-green-600 hover:bg-green-700 transition duration-150 ease-in-out"
               onClick={() => setMobileNavOpen(false)}
             >
-              Sign up
+              Sign in
             </Link>
           </li>
         </ul>
