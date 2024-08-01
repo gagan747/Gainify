2130d45b2dec6b9195cac50a7ef991dd9fdd9719
gurjot74 <gagan.singh@tothenew.com>
2024-08-02 03:01:17 +0530

    added opacity in body when mobile nav is open

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -35,96 +35,106 @@ export default function MobileMenu() {
     return () => document.removeEventListener("keydown", keyHandler);
   });
 
-  return (
-    <div className="md:hidden">
-      {/* Hamburger button */}
-      <button
-        ref={trigger}
-        className={`hamburger ${mobileNavOpen && "active"}`}
-        aria-controls="mobile-nav"
-        aria-expanded={mobileNavOpen}
-        onClick={() => setMobileNavOpen(!mobileNavOpen)}
-      >
-        <span className="sr-only">Menu</span>
-        <svg
-          className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect y="4" width="24" height="2" rx="1" />
-          <rect y="11" width="24" height="2" rx="1" />
-          <rect y="18" width="24" height="2" rx="1" />
-        </svg>
-      </button>
+    useEffect(() => {
+					if (mobileNavOpen) {
+						document.body.style.overflow = 'hidden'; // Stop body scrolling
+					} else {
+						document.body.style.overflow = 'auto'; // Allow body scrolling
+					}
 
-      {/*Mobile navigation */}
-      <nav
-        id="mobile-nav"
-        ref={mobileNav}
-        className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
-        style={
-          mobileNavOpen
-            ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 }
-            : { maxHeight: 0, opacity: 0.8 }
-        }
-      >
-        <ul className="bg-gray-800 px-4 py-2">
-          <li>
-            <Link
-              href="/#testimonials"
-              className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              About us
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/#services"
-              className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Our Services
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/#services"
-              className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Contact Us
-            </Link>
-          </li>{" "}
-          <li>
-            <Link
-              href="/#services"
-              className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Terms of Service
-            </Link>
-          </li>{" "}
-          <li>
-            <Link
-              href="/#services"
-              className="flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Privacy Policy
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/signin"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-green-600 hover:bg-green-700 transition duration-150 ease-in-out"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Sign in
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+					// Cleanup function to reset overflow when unmounted
+					return () => {
+						document.body.style.overflow = 'auto'; // Ensure scrolling is allowed when component unmounts
+					};
+				}, [mobileNavOpen]);
+
+
+  return (
+			<div className='md:hidden'>
+				{/* Hamburger button */}
+				<button
+					ref={trigger}
+					className={`hamburger ${mobileNavOpen && 'active'}`}
+					aria-controls='mobile-nav'
+					aria-expanded={mobileNavOpen}
+					onClick={() => setMobileNavOpen(!mobileNavOpen)}>
+					<span className='sr-only'>Menu</span>
+					<svg
+						className='w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out'
+						viewBox='0 0 24 24'
+						xmlns='http://www.w3.org/2000/svg'>
+						<rect y='4' width='24' height='2' rx='1' />
+						<rect y='11' width='24' height='2' rx='1' />
+						<rect y='18' width='24' height='2' rx='1' />
+					</svg>
+				</button>
+
+				{/*Mobile navigation */}
+				<nav
+					id='mobile-nav'
+					ref={mobileNav}
+					className='absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out'
+					style={
+						mobileNavOpen
+							? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 }
+							: { maxHeight: 0, opacity: 0.8 }
+					}>
+					<ul className='bg-gray-800 px-4 py-2'>
+						<li>
+							<Link
+								href='/#testimonials'
+								className='flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center'
+								onClick={() => setMobileNavOpen(false)}>
+								About us
+							</Link>
+						</li>
+						<li>
+							<Link
+								href='/#services'
+								className='flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center'
+								onClick={() => setMobileNavOpen(false)}>
+								Our Services
+							</Link>
+						</li>
+						<li>
+							<Link
+								href='/#services'
+								className='flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center'
+								onClick={() => setMobileNavOpen(false)}>
+								Contact Us
+							</Link>
+						</li>{' '}
+						<li>
+							<Link
+								href='/#services'
+								className='flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center'
+								onClick={() => setMobileNavOpen(false)}>
+								Terms of Service
+							</Link>
+						</li>{' '}
+						<li>
+							<Link
+								href='/#services'
+								className='flex font-medium w-full text-green-600 hover:text-gray-200 py-2 justify-center'
+								onClick={() => setMobileNavOpen(false)}>
+								Privacy Policy
+							</Link>
+						</li>
+						<li>
+							<Link
+								href='/signin'
+								className='font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-green-600 hover:bg-green-700 transition duration-150 ease-in-out'
+								onClick={() => setMobileNavOpen(false)}>
+								Sign in
+							</Link>
+						</li>
+					</ul>
+					<div
+						className='h-screen'
+						style={{
+							backgroundColor: 'rgba(0, 0, 0, 0.7)',
+						}}></div>
+				</nav>
+			</div>
+		);
 }
